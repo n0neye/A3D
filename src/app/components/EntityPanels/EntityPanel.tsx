1abe487ef58b8c6e281f0be55cd2919a0f75a8f6
YI <yi@nontech.net>
2025-04-13 10:46:06 +0800

    minor

--- src/app/components/EntityPanels/EntityPanel.tsx
+++ src/app/components/EntityPanels/EntityPanel.tsx
@@ -11,30 +11,21 @@ import CharacterEditPanel from '../CharacterEditPanel';
 const EntityPanel: React.FC = () => {
   const { selectedEntity } = useEditorEngine();
 
-  useEffect(() => {
-    console.log('selectedEntity', selectedEntity);
-  }, [selectedEntity]);
-
   // Show panel for both generative objects and lights
   if (!selectedEntity) return null;
-  if (selectedEntity.getEntityType() !== 'light' && selectedEntity.getEntityType() !== 'generative' && selectedEntity.getEntityType() !== 'character') {
-    return null;
+
+  if (selectedEntity.type == 'light' || selectedEntity.type == 'generative' || selectedEntity.type == 'character') {
+    return (
+      <div
+        id="entity-panel" className="fixed z-10 panel left-1/2 -translate-x-1/2 bottom-4 min-w-[150px]">
+        {selectedEntity instanceof GenerativeEntity && <GenerativeEntityPanel entity={selectedEntity} />}
+        {selectedEntity instanceof LightEntity && <LightEntityPanel entity={selectedEntity} />}
+        {selectedEntity instanceof CharacterEntity && <CharacterEditPanel entity={selectedEntity} />}
+      </div>
+    );
   }
 
-  return (
-    <div
-      id="entity-panel"
-      className="fixed z-10 panel left-1/2 bottom-4 "
-      style={{
-        transform: 'translateX(-50%)', // Center horizontally
-        minWidth: '150px',
-      }}
-    >
-      {selectedEntity instanceof GenerativeEntity && <GenerativeEntityPanel entity={selectedEntity} />}
-      {selectedEntity instanceof LightEntity && <LightEntityPanel entity={selectedEntity} />}                                                                        
-      {selectedEntity instanceof CharacterEntity && <CharacterEditPanel entity={selectedEntity} />}
-    </div>
-  );
+  return null;
 };
 
 export default EntityPanel; 
